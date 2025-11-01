3fa2cd18a4abad3ec3b4d33d03c538f25dfda242
Lucas Lim <lucaslim9729@gmail.com>
2025-11-01 10:06:46 -0400

    add button labrary

--- src/pages/Social.tsx
+++ src/pages/Social.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ArrowLeft } from "lucide-react";
 import { MessageSquare, Users } from "lucide-react";
